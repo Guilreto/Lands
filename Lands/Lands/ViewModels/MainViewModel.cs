8f2437883401fce79978fca4cdd711095565f5c2
Guillermo <guilreto92@gmail.com>
2018-04-19 14:12:21 +0200

    LoginViewModel LoginPage
    Añadida la View Model de LoginPage y esta adaptada

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -9,5 +9,18 @@ namespace Lands.ViewModels
 {
 	public class MainViewModel
 	{
-	}
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get; set;
+        }
+        #endregion
+
+        #region Constructores
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
 }
