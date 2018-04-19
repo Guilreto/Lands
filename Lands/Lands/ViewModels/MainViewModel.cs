541f0b2c908bd4f6680aae03dd4854ce8fd431d2
Guillermo <guilreto92@gmail.com>
2018-04-19 12:16:40 +0200

    MVVM y View Login
    Añadido View Login y gestion de proyecto en MVVM

---
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Lands.ViewModels
+{
+	public class MainViewModel
+	{
+	}
+}
